2ac78916046e1eab813f9eb8a193fa1a5b225429
zackrut <60199896+zackrut@users.noreply.github.com>
2020-02-12 00:46:14 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            times = input("Enter amount of barks.\n\n")
+            int Bark;
+            int GetTag;
+        }
+            public class Dog
+            {
+                private string name;
+                private string owner;
+                private int age;
+                private enum Gender;
+                public Dog(string name, string owner, int age, enum Gender)
+                {
+                string name = Spot;
+                string owner = Zack;
+                int age = 10;
+                enum Gender =
+                {
+                    Male,
+                    Female
+                }
+                    
+                }
+                int Bark(int n)
+                {
+                    n = 0;
+
+                    while (times >= n)
+                    {
+                    Console.WriteLine("Woof!\n");
+                    n++;
+                    }
+
+                }
+
+                int GetTag()
+                {
+                    Console.WriteLine("If lost, call %s. ", owner);
+                    Console.WriteLine("%d name is ", gender);
+                    Console.WriteLine("%s ", name);
+                    Console.WriteLine("and %d is ", gender);
+                    Console.WriteLine(" %d years old.\n", age)
+                }
+
+                
+
+            }
+            
+        }
+
+        
+    }
